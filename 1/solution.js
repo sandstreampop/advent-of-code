e9010f0aa24064b64e5e6fe8aa0cba30aa947c04
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-12-01 11:16:19 +0100

    solution to day 1

---
+++ 1/solution.js
@@ -0,0 +1,19 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const lines = input.split("\n").map((line) => Number(line));
+
+const { answer } = lines.reduce(
+  ({ answer, lastLine }, currentLine) => ({
+    answer:
+      lastLine !== null && currentLine > lastLine
+        ? answer + 1
+        : answer,
+    lastLine: currentLine,
+  }),
+  {
+    answer: 0,
+    lastLine: null,
+  }
+);
+
+console.log(answer);
