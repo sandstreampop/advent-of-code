b3f9fa2cfc2912babba6366834ad9ca4bcfd4c80
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-12-04 23:25:14 +0100

    feat: day 2, solution 1

---
+++ 2/solution-part-1.js
@@ -0,0 +1,18 @@
+export function Solution1(input) {
+  const lines = input.split("\n");
+
+  const { forward, up, down } = lines.reduce((result, line) => {
+		const [direction, distance] = line.split(" ");
+		result[direction] += Number(distance);
+		return result;
+	}, {
+    forward: 0,
+    up: 0,
+		down: 0,
+  });
+
+	const depth = down - up;
+	const answer = forward * depth;
+
+	return answer;
+}
--- mod.js
+++ mod.js
@@ -1,6 +1,11 @@
-import { Solution1 } from "./1/solution-part-1.js";
-import { Solution2 } from "./1/solution-part-2.js";
+import { Solution1 as Day1Solution1 } from "./1/solution-part-1.js";
+import { Solution2 as Day1Solution2 } from "./1/solution-part-2.js";
+import { Solution1 as Day2Solution1 } from "./2/solution-part-1.js";
 
-const input1 = await Deno.readTextFile("./1/input.txt");
-console.log(`answer to day 1, part 1: ${Solution1(input1)}`);
-console.log(`answer to day 1, part 2: ${Solution2(input1)}`);
+const day1Input = await Deno.readTextFile("./1/input.txt");
+console.log(`answer to day 1, part 1: ${Day1Solution1(day1Input)}`);
+console.log(`answer to day 1, part 2: ${Day1Solution2(day1Input)}`);
+
+const day2Input = await Deno.readTextFile("./2/input.txt");
+console.log(`answer to day 2, part 1: ${Day2Solution1(day2Input)}`);
+
